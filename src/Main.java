735bfd1dca0bc18140f2ae8196efdfae31930c56
Диана <ashkhar.ik@mail.ru>
2026-02-22 20:12:56 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,16 @@ public class Main {
         } else if (clientOs == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
+
+        ///#2
+        int clientOS = 1;
+        int clientOsYear = 2026;
+        if (clientOS == 0 && clientOsYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке.");
+        } else if (clientOS == 1 && clientOsYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("Установите версию приложения для " + (clientOS == 0 ? "iOS" : "Android") + " по ссылке:");
+        }
     }
 }
